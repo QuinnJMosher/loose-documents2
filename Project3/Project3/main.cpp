1668c4bca506f5feb4a34dbb5c4a0c7c02b49e4b
QuinnJMosher <Darkstaro59@gmail.com>
2014-11-24 14:05:17 -0800

    maps and sets

--- Project3/Project3/main.cpp
+++ Project3/Project3/main.cpp
@@ -1,22 +1,52 @@
 #include <iostream>
-#include "Queue.h"
-#include "Queue.cpp"
+#include <fstream>
+#include <string>
+#include <map>
+#include <set>
+
+using namespace std;
 
 void main() {
 
-	Queue<int> queue = Queue<int>();
-	int value = 10;
-	queue.push(value);
-	value = 20;
-	queue.push(value);
-	value = 30;
-	queue.push(value);
+	fstream inFile = fstream("AesopsFables.txt", fstream::in);
+	if (!inFile.is_open()) {
+		throw("input file not found");
+	}
 
-	std::cout << queue.isEmpty() << "\n";
-	std::cout << queue.pop() << "\n";
-	std::cout << queue.pop() << "\n";
-	std::cout << queue.pop() << "\n";
-	std::cout << queue.isEmpty() << "\n";
+	fstream outFile = fstream("wordcounter.txt", fstream::out);
+	if (!outFile.is_open()) {
+		throw("output file open failure");
+	}
+
+	map<string, unsigned int> wordCount = map<string, unsigned int>();
+	set<string> wordList = set<string>();
+
+	string currentWord = "";
+	inFile >> currentWord;
+	int i = 0;
+
+	while (!inFile.eof()) {
+		if (wordCount.find(currentWord) == wordCount.end()) {
+			wordCount[currentWord] = 1;
+		} else {
+			wordCount[currentWord]++;
+		}
+
+		if (wordList.find(currentWord) == wordList.end()) {
+			wordList.insert(currentWord);
+		}
+
+		i++;
+		cout << i << " " << currentWord << endl;
+
+		inFile >> currentWord;
+	}
+
+	system("cls");
+
+	for (int i = 0; i < wordCount.size(); i++) {
+		//cout << wordList.find(wordList.begin() + i) << " " << wordCount.at(wordList.find(wordList.begin() + i)) << endl;
+	}
 
 	system("pause");
 }
